d1ad734cf1abb0c5d29ff0c6e3cf242859ff58fe
Yusuke Takahashi <minustars1124@yahoo.co.jp>
2023-03-08 16:22:47 +0900

    設定ファイルから環境変数を読み込む処理を実装

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    VITE_TRIAL_COUNT: string;
+    VITE_WORD_LENGTH: string;
+    VITE_ANSWER: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import NoodleContents from '../components/NoodleContents.vue'
+const trialCount : number = Number(import.meta.env.VITE_TRIAL_COUNT)
+const wordLength : number = Number(import.meta.env.VITE_WORD_LENGTH)
+const answerFormula : Array<string> = [...import.meta.env.VITE_ANSWER]
 </script>
 
 <template>
   <main>
-    <NoodleContents :trial-count="6" :word-length="8"
-                    :answer-formula="['3', '+', '2', '*', '8', '=', '1', '9']"/>
+    <NoodleContents :trial-count="trialCount" :word-length="wordLength"
+                    :answer-formula="answerFormula"/>
   </main>
 </template>
